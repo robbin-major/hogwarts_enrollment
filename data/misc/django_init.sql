ac868d89cf9ec5e78f27122c0be497136a1dac91
Robbin Major <robbinmajor@Robbins-MacBook-Pro.local>
2022-01-21 21:00:26 -0600

    first commit

---
+++ data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS hogwarts_enrollment_db;
+CREATE DATABASE hogwarts_enrollment_db;
+
+\c hogwarts_enrollment_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
